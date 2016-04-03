a916143fbe57a0dd511f1a0aa42705e84c9bc450
Vinícius Antonio Nolli Sasso <vinicius_sasso@yahoo.com.br>
2016-04-03 20:42:44 -0300

    Added files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,37 @@
 // Documento JavaScript
 
-function menu(n) {
-if(n.length < 1) { return; }
-if(document.getElementById(n).style.display == "none") { document.getElementById(n).style.display = "block"; }
-else { document.getElementById(n).style.display = "none"; }
+function menu(n,a) {
+if(n.length < 1) {
+	return; }
+if(document.getElementById(n).style.display == "none") {
+	document.getElementById(n).style.display = "inline-block";
+	} else {
+		document.getElementById(n).style.display = "none";
+	}
 }
 
-function esconde(d) {
-document.getElementById(d).style.display == "none";	
+function esconde(n) {
+document.getElementById(n).style.display == "none";	
 }
+
+function fundoMenu (id) {
+
+var menuMobileExpandiu = document.getElementsByClassName('in');
+if (menuMobileExpandiu.length > 0) {
+	document.getElementById(id).style.display = "none";
+	} else {
+		document.getElementById(id).style.display = "block";
+	}	
+
+}
+
+/*	
+if(id.length < 1) {
+	return; }
+if(document.getElementById(id).style.display == "block") {
+	document.getElementById(id).style.display = "none";
+	} else {
+		document.getElementById(id).style.display = "block";
+	}	
+}
+*/
