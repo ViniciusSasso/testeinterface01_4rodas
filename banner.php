9ac43f6b017f06aacbe8ec0f69ad51b0b53fad77
Vinícius Antonio Nolli Sasso <vinicius_sasso@yahoo.com.br>
2016-04-03 21:47:10 -0300

    Added files via upload

--- banner.php
+++ banner.php
@@ -1,11 +1,11 @@
 <div id="Banner" class="container-fluid">
 	<!-- Desktop -->
-	<div class="row hidden-sm hidden-xs">
+	<div class="row hidden-xs">
     <img class="img-responsive center-block bannerPrincipal" src="<?php bloginfo('template_directory');?>/imagens/banner-topo.png" />
     </div>
 
 	<!-- telas menores -->
-	<div class="row visible-xs visible-sm bannerMobile">
+	<div class="row visible-xs bannerMobile">
     <div class="col-xs-12 col-sm-12">
     <img class="img-responsive center-block bannerPrincipal" src="<?php bloginfo('template_directory');?>/imagens/banner-topo.png" />
     </div>
